a6ecd50c5cbb4ea72f455fadb8def50b6ddea6f3
Paul <paulbyrne99@rocketmail.com>
2020-09-04 00:46:25 +0100

    basic skeleton added

---
+++ src/ai.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep  3 23:21:45 2020
+
+@author: paulb
+"""
+
+# FIRST TIME
+
+# Import datasets
+
+# Clean data and combine different leagues
+
+# Work out predicted points 
+
+# Find optimal team by pred. points
+
+# Confirm team
+
+# EACH WEEK
+
+# Pull latest data
+
+# Work out predicted points 
+
+# Weigh up predicted points vs cost of transfers
+
+# Make transfers 
+
+# Confirm team
