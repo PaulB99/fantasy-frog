c008d2ccd56b838ad18efbedb073938314902507
Paul <paulbyrne99@rocketmail.com>
2020-09-10 03:12:22 +0100

    algorithm changes reverted

--- src/ai.py
+++ src/ai.py
@@ -114,12 +114,19 @@ for i in range(1, total+1): #total+1
         # MAKE PREDICTIONS
         last_season_weight = math.log10(11-gameweek)  # Weight of past season should decrease over time
         if(form == 0.0): # if no form
+            pred_5 = (ppg  * (diffi_5/average))
+            pred_next = (ppg * (diffi_next/average))
+        else:
+            pred_5 = ((ppg + form) /2 * (diffi_5/average))
+            pred_next = ((ppg + form) /2 * (diffi_next/average))
+        '''
+        if(form == 0.0): # if no form
             pred_5 = (((ppg + (last_total / 38)) /2)  * (diffi_5/average))
             pred_next = (((ppg + (last_total / 38)) /2) * (diffi_next/average))
         else:
             pred_5 = ((ppg + ((last_total / 38) * last_season_weight) + (form * (1- last_season_weight))) /3 * (diffi_5/average))
             pred_next = ((ppg + ((last_total / 38) * last_season_weight) + (form * (1- last_season_weight))) /3 * (diffi_next/average))
-        
+            '''
         # Add to array
         preds_5.append(pred_5)
         preds_next.append(pred_next)
@@ -277,7 +284,6 @@ for f in fwds:
     for s in starting_team:  # Find lowest scoring player
         if(preds_next[s[1]] < preds_next[lowest[1]]):
             lowest = s
-    print(lowest)
     if (preds_next[f[1]] > preds_next[lowest[1]]):
         bench.append(lowest)
         new_team = [n for n in starting_team if n[0] != lowest[0]]
